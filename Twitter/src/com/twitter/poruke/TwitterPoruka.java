a9021cf06cbc10b838ab3b5ae3cc388b71b13b0d
ivan996 <ivan.stanimirovic692@gmail.com>
2017-04-03 10:43:00 +0200

    Dodat JavaDoc u klasu TwitterPruka

--- Twitter/src/com/twitter/poruke/TwitterPoruka.java
+++ Twitter/src/com/twitter/poruke/TwitterPoruka.java
@@ -1,30 +1,70 @@
 package com.twitter.poruke;
-
+/**
+ * Klasa koja definise korisnika i poruke 
+ * @author Ivan Stanimirovic
+ * @version1.0
+ */
 public class TwitterPoruka {
-
+	/**
+	 * atribut korisnik koji sadzrzi String vrednost, i oznacava korisnika 
+	 */
 	private String korisnik;
+	/**
+	 * atribut poruka koja sadrzi String vrednost, i oznacava poruku koju korisnik unosi
+	 */
 	private String poruka;
-
+	
+	/**
+	 * metoda vraca korisnika
+	 * @return vraca String vrednost koja oznacava ime korisnika
+	 */
 	public String getKorisnik() {
 		return korisnik;
 	}
-
+	
+	/**
+	 * metoda postavlja vrednost korisnika na unetu vrednost
+	 * @param korisnik oznacava String koji se postavlja kao vrednost privatnog atributa korisnik, koji ne sme da bude:
+	 * <ul>
+	 * <li>null</li>
+	 * <li>prazan String</li>
+	 * </ul>
+	 * @throws java.lang.RuntimeException ako parametar korisnik ne odgovara uslovima
+	 */
 	public void setKorisnik(String korisnik) {
 		if (korisnik == null || !korisnik.isEmpty())
 			throw new RuntimeException("Ime korisnika mora biti uneto");
 		this.korisnik = korisnik;
 	}
-
+	
+	/**
+	 * Metoda koja vraca poruku
+	 * @return String, sadrzaj privatnog atributa poruka
+	 */
 	public String getPoruka() {
 		return "poruka";
 	}
-
+	
+	/**
+	 * metoda postavlja vrednost privatnog atrbuta poruka na unetu vrednost
+	 * @param korisnik oznacava String koji se postavlja kao vrednost privatnog atributa poruka, koji ne sme da bude:
+	 * <ul>
+	 * <li>null</li>
+	 * <li>prazan String</li>
+	 * <li>String veci od 140 znakova</li>
+	 * </ul>
+	 * @throws java.lang.RuntimeException ako parametar poruka ne odgovara uslovima
+	 */
 	public void setPoruka(String poruka) {
 		if (this.poruka == null || this.poruka == new String("") || this.poruka.length() > 140)
 			throw new RuntimeException("Poruka mora biti uneta i mora imati najvise 140 znakova");
 		this.poruka = poruka;
 	}
-
+	
+	/**
+	 * Metoda vraca String sa sadrzajem atributa Korisnik i Poruka
+	 * @return String vrednost
+	 */
 	public String toString() {
 		return "KORISNIK:" + korisnik + " PORUKA:" + poruka;
 	}
